50750c5d92c568e3737c996582780d0d2f9fc6de
GomaSenbei <148687287+GomaSenbei@users.noreply.github.com>
2023-10-22 21:28:03 +0900

    Update const.js

--- js/const.js
+++ js/const.js
@@ -1,1 +1,6 @@
-WSURL = 'ws://goma-catan.onrender.com';
+//WSURL = 'wss://catasorasp.omatz.cf';
+//WSURL = 'wss://cataso2.omatz.cf';
+//WSURL = 'wss://cataso-omatz.herokuapp.com';
+//WSURL = 'ws://localhost:5000';
+
+WSURL = 'wss://neocataso.onrender.com';
